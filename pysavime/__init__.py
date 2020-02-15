dcfaef6d2be53258e4865fc6c93577ca22b8bf07
Daniel <danielnascramos@gmail.com>
2020-02-15 18:40:13 -0300

    Make import easier.

--- pysavime/__init__.py
+++ pysavime/__init__.py
@@ -1,3 +1,3 @@
 from pysavime.savime.client import Client
-import pysavime.schema.define as savime_define
-import pysavime.misc.commands as savime_operator
+import pysavime.schema.define as define
+import pysavime.misc.commands as operator
